2940db02195c1c6cdada1f07b8be59a874852998
Yuvaraj Poduri <118831011+yuvarajpoduri@users.noreply.github.com>
2025-09-08 20:09:12 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://tvtracker-j7g5.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
